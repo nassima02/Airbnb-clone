3ce94e99d387f2581a0785a42b9d0abb6790d7c0
myriamsierra <125214125+myriamsierra@users.noreply.github.com>
2024-03-14 11:33:40 +0100

    Update admin-section.jsx

--- src/components/header/header-childs/top-header/admin-section.jsx
+++ src/components/header/header-childs/top-header/admin-section.jsx
@@ -1,21 +1,22 @@
+
 import React from "react";
 import LanguageIcon  from '@mui/icons-material/Language';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MenuIcon from '@mui/icons-material/Menu';
+import {Box} from "@mui/material";
 import './../../../../index.scss';
 
 const AdminSection = () => {
     return(
-        <div className="admin-section">
+        <Box className="admin-section">
             <p>Mettre mon logement sur Airbnb</p>
             <LanguageIcon className="admin-section__language" fontSize="small"/>
             <div className="admin-section__button">
                 <MenuIcon className="admin-section__button__menu" fontSize="small"/>
                 <AccountCircleIcon className="admin-section__button__profil" fontSize="large" />
             </div>
-        </div>
+        </Box>
     )
 }
 
 export default AdminSection
-
